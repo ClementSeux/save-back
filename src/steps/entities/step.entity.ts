15395e53532ebebd3d50907ef70e85cc6ed75784
Clément SEUX <clement.seux@axxes.fr>
2024-05-19 16:37:15 +0200

    ```text refactor: Update Step entity to use cartId instead of cart
    The code changes in `step.entity.ts` modify the `Step` entity by replacing the `cart` property with `cartId`. This change improves the clarity and consistency of the code by using a more descriptive name for the foreign key column.

--- src/steps/entities/step.entity.ts
+++ src/steps/entities/step.entity.ts
@@ -19,7 +19,7 @@ export class Step {
   // Cart
   @ApiProperty()
   @ManyToOne(() => Cart, (cart) => cart.steps)
-  cart: Cart;
+  cartId: Cart;
 
   @ApiProperty()
   @Column({ type: 'varchar', length: 120 })
