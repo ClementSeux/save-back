5bd905f2f13b94ab189e4447be0c77eb00db12ef
Clément SEUX <clement.seux@axxes.fr>
2024-05-23 12:31:33 +0200

    refactor: Update UserCartsModule to import UserCart entity
    The code changes in `user_carts.module.ts` add the `UserCart` entity to the `imports` array of the `UserCartsModule`. This change ensures that the `UserCart` entity is properly registered with the TypeORM module, allowing it to be used in the module's services and controllers.

--- src/user_carts/user_carts.module.ts
+++ src/user_carts/user_carts.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserCartsService } from './user_carts.service';
 import { UserCartsController } from './user_carts.controller';
+import { UserCart } from './entities/user_cart.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [UserCartsController],
   providers: [UserCartsService],
+  imports: [TypeOrmModule.forFeature([UserCart])],
 })
 export class UserCartsModule {}
