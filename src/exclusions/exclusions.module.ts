4da250c88fa45ea9604e2a063a667c55a693a151
Clément SEUX <clement.seux@axxes.fr>
2024-05-24 22:34:52 +0200

    refactor: Add TypeOrmModule.forFeature to ExclusionsModule
    The code changes in `exclusions.module.ts` add `TypeOrmModule.forFeature([Exclusion])` to the `imports` array of the `ExclusionsModule`. This enhancement allows the module to use the `Exclusion` entity provided by TypeORM, enabling database operations related to exclusions.

--- src/exclusions/exclusions.module.ts
+++ src/exclusions/exclusions.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExclusionsService } from './exclusions.service';
 import { ExclusionsController } from './exclusions.controller';
+import { Exclusion } from './entities/exclusion.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Exclusion])],
   controllers: [ExclusionsController],
   providers: [ExclusionsService],
 })
