d10e3370c89dc8b72ba55a451239cb4e1a50dc65
Clément SEUX <clement.seux@axxes.fr>
2024-05-24 23:05:17 +0200

    refactor: Update Exclusion entity to use direct values for userId and stepId

--- src/exclusions/entities/exclusion.entity.ts
+++ src/exclusions/entities/exclusion.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryColumn, ManyToOne } from 'typeorm';
+import { Entity, PrimaryColumn, ManyToOne, JoinColumn } from 'typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { Step } from 'src/steps/entities/step.entity';
 import { ApiProperty } from '@nestjs/swagger';
@@ -6,16 +6,14 @@ import { ApiProperty } from '@nestjs/swagger';
 @Entity()
 export class Exclusion {
   @ApiProperty()
-  @PrimaryColumn()
-  userId: number;
-
-  @ApiProperty()
-  @PrimaryColumn()
-  stepId: number;
-
+  @PrimaryColumn({ type: 'int', name: 'user_id' })
   @ManyToOne(() => User, (user) => user.exclusions)
+  @JoinColumn({ name: 'user_id' })
   user: User;
 
+  @ApiProperty()
+  @PrimaryColumn({ type: 'int', name: 'step_id' })
   @ManyToOne(() => Step, (step) => step.exclusions)
+  @JoinColumn({ name: 'step_id' })
   step: Step;
 }
