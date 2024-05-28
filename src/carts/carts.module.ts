dd0eae322c554d8dacbbe3d6c68590c17c4c7696
Clément SEUX <clement.seux@axxes.fr>
2024-05-28 11:05:22 +0200

    refactor: Update carts module imports
    The code changes in carts.module.ts update the imports to include the User entity and UserService from the user module. This refactor enhances the functionality of the carts module by incorporating user-related functionality.

--- src/carts/carts.module.ts
+++ src/carts/carts.module.ts
@@ -3,10 +3,12 @@ import { CartService } from './carts.service';
 import { CartsController } from './carts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cart } from './entities/cart.entity';
+import { User } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cart])],
+  imports: [TypeOrmModule.forFeature([Cart, User])],
   controllers: [CartsController],
-  providers: [CartService],
+  providers: [CartService, UserService],
 })
 export class CartsModule {}
